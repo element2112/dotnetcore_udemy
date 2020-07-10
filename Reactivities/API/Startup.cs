62acc2e54f6c49e489e5c4c10079351bc4eb9d98
B S <bs009@live.com>
2020-07-10 15:17:02 -0400

    db udpate

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -23,13 +25,16 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) // order not important
         {
+            services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)  // order is important
         {
             if (env.IsDevelopment())
             {
---
+++ Reactivities/Domain/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    // code first: write code first then create the db scripts
+    public class Value
+    {
+        public int Id { get; set; } // entity framework will automatically see an Id property and use this as the primary key
+        public string Name { get; set; }
+
+
+    }
+}
